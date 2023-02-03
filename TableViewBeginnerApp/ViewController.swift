f0229ea4ab40878411e5568943fa73499dda004d
handenuryavas <hande.nur038@gmail.com>
2023-02-03 14:11:52 +0300

    Initial Commit

---
+++ TableViewBeginnerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewBeginnerApp
+//
+//  Created by Hande Nur Yavaş on 3.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
